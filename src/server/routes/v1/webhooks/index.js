cbda1fb2cd44e1f7a8434b9563078cad48259e20
ajays97 <ajaysrinivas530@gmail.com>
2019-06-11 20:31:55 +0530

    Add webhooks support for author related books

--- src/server/routes/v1/webhooks/index.js
+++ src/server/routes/v1/webhooks/index.js
@@ -30,6 +30,81 @@ db.collection('books')
     routes.post('/', (req, res) => {
       const agent = new WebhookClient({ request: req, response: res });
 
+      function sameAuthorBooks() {
+        const contextParams = agent.context.get('book_description').parameters;
+        const bookId = contextParams.book;
+        return db
+          .collection('books')
+          .where('author_name', '==', contextParams.bookAuthor)
+          .get()
+          .then(snapshot => {
+            let books = [];
+            snapshot.forEach(doc => {
+              let book = {};
+              book.title = doc.data().title;
+              book.bookId = doc.data()['book_id'];
+              book.imageUrl = doc.data()['m_image_url'];
+              book.authorName = doc.data()['author_name'];
+              book.amazonPrice = doc.data()['amazon_price'];
+              book.authorId = doc.data()['author_id'];
+              books.push(book);
+            });
+            agent.context.set({
+              name: 'book_recommendations',
+              lifespan: 1,
+              parameters: {
+                mode: 'CB',
+                book: req.body.queryResult.parameters.book,
+                bookTitle: contextParams.bookTitle,
+                bookRating: contextParams.bookRating,
+                bookAuthor: contextParams.bookAuthor,
+                books: books
+              }
+            });
+            agent.add(`Here are some more books by ${contextParams.bookAuthor}:-`);
+          })
+          .catch(err => {
+            agent.add('Server error. Please try something else or after sometime.');
+            console.log(err);
+          });
+      }
+
+      function addBookToCart() {
+        const contextParams = agent.context.get('book_description').parameters;
+        const bookId = contextParams.book;
+        let cartBook = {};
+        cartBook['book_id'] = bookId;
+        cartBook['author'] = contextParams.bookAuthor;
+        cartBook['goodreads_rating'] = contextParams.bookRating;
+        cartBook['mrp'] = contextParams.bookPrice;
+        cartBook['price'] = contextParams.bookPrice;
+        cartBook['s_image_url'] = contextParams.imageUrl;
+        cartBook['title'] = contextParams.bookTitle;
+        db.collection('users')
+          .doc('ajaysrinivas530@gmail.com')
+          .collection('cart')
+          .doc(bookId)
+          .set(cartBook);
+        agent.add(`I have added ${contextParams.bookTitle} to the cart. Have fun and happy reading.`);
+      }
+
+      function clearCart() {
+        db.collection('users')
+          .doc('ajaysrinivas530@gmail.com')
+          .collection('cart')
+          .get()
+          .then(snapshot => {
+            agent.add('I have cleared your cart for you. Happy shopping something new.');
+            snapshot.forEach(book => {
+              db.collection('users')
+                .doc('ajaysrinivas530@gmail.com')
+                .collection('cart')
+                .doc(book.id)
+                .delete();
+            });
+          });
+      }
+
       function similarBooks() {
         const contextParams = agent.context.get('book_description').parameters;
         const bookTitle = contextParams.bookTitle;
@@ -49,6 +124,8 @@ db.collection('books')
       }
 
       function describeBook() {
+        console.log('received book:', req.body.queryResult.parameters.book);
+
         return db
           .collection('books')
           .doc(req.body.queryResult.parameters.book)
@@ -63,7 +140,10 @@ db.collection('books')
                 parameters: {
                   book: req.body.queryResult.parameters.book,
                   bookTitle: doc.data()['title'],
-                  bookRating: doc.data()['goodreads_rating']
+                  bookRating: doc.data()['goodreads_rating'],
+                  bookAuthor: doc.data()['author_name'],
+                  bookPrice: doc.data()['amazon_price'],
+                  imageUrl: doc.data()['s_image_url']
                 }
               });
               agent.add(`${doc.data()['short_description']}`);
@@ -75,95 +155,98 @@ db.collection('books')
           });
       }
 
-      console.log(req.body);
-      if (req.body.rectype === 'CB') {
-        const contentBasedRecommendation = predictWithContentBased(X, BOOKS_IN_LIST, bookTitle);
-        res.status(200).send(sliceAndDice(contentBasedRecommendation, BOOKS_BY_ID, 10, true));
-      } else if (req.body.rectype === 'LIN') {
-        const ME_USER_ID = req.body.userId;
-        const ME_USER_EMAIL = req.body.userEmail;
+      // console.log(req.body);
+      // if (req.body.rectype === 'CB') {
+      //   const contentBasedRecommendation = predictWithContentBased(X, BOOKS_IN_LIST, bookTitle);
+      //   res.status(200).send(sliceAndDice(contentBasedRecommendation, BOOKS_BY_ID, 10, true));
+      // } else if (req.body.rectype === 'LIN') {
+      //   const ME_USER_ID = req.body.userId;
+      //   const ME_USER_EMAIL = req.body.userEmail;
 
-        // Fetch user submitted book ratings
-        db.collection('ratings')
-          .get()
-          .then(snapshot => {
-            let ratings = [];
-            snapshot.forEach(doc => {
-              let rating = { ...doc.data() };
-              ratings.push(rating);
-            });
+      //   // Fetch user submitted book ratings
+      //   db.collection('ratings')
+      //     .get()
+      //     .then(snapshot => {
+      //       let ratings = [];
+      //       snapshot.forEach(doc => {
+      //         let rating = { ...doc.data() };
+      //         ratings.push(rating);
+      //       });
 
-            let ME_USER_RATINGS = ratings.filter(rating => {
-              return rating['user_email'] === ME_USER_EMAIL;
-            });
+      //       let ME_USER_RATINGS = ratings.filter(rating => {
+      //         return rating['user_email'] === ME_USER_EMAIL;
+      //       });
 
-            console.log('ME_USER_RATINGS:', ME_USER_RATINGS);
+      //       console.log('ME_USER_RATINGS:', ME_USER_RATINGS);
 
-            const { ratingsGroupedByUser, ratingsGroupedByBook } = prepareRatings([...ME_USER_RATINGS, ...ratings]);
-            const meUserRatings = ratingsGroupedByUser[ME_USER_ID];
-            const linearRegressionBasedRecommendation = predictWithLinearRegression(X, BOOKS_IN_LIST, meUserRatings);
-            res.send(sliceAndDice(linearRegressionBasedRecommendation, BOOKS_BY_ID, 10, true));
-          });
-      } else if (req.body.rectype === 'ICF') {
-        const ME_USER_ID = req.body.userId;
-        const ME_USER_EMAIL = req.body.userEmail;
+      //       const { ratingsGroupedByUser, ratingsGroupedByBook } = prepareRatings([...ME_USER_RATINGS, ...ratings]);
+      //       const meUserRatings = ratingsGroupedByUser[ME_USER_ID];
+      //       const linearRegressionBasedRecommendation = predictWithLinearRegression(X, BOOKS_IN_LIST, meUserRatings);
+      //       res.send(sliceAndDice(linearRegressionBasedRecommendation, BOOKS_BY_ID, 10, true));
+      //     });
+      // } else if (req.body.rectype === 'ICF') {
+      //   const ME_USER_ID = req.body.userId;
+      //   const ME_USER_EMAIL = req.body.userEmail;
 
-        let ratings = [];
+      //   let ratings = [];
 
-        db.collection('ratings')
-          .get()
-          .then(snapshot => {
-            snapshot.forEach(doc => {
-              let rating = { ...doc.data() };
-              ratings.push(rating);
-            });
+      //   db.collection('ratings')
+      //     .get()
+      //     .then(snapshot => {
+      //       snapshot.forEach(doc => {
+      //         let rating = { ...doc.data() };
+      //         ratings.push(rating);
+      //       });
 
-            let ME_USER_RATINGS = ratings.filter(rating => {
-              return rating['user_email'] === ME_USER_EMAIL;
-            });
+      //       let ME_USER_RATINGS = ratings.filter(rating => {
+      //         return rating['user_email'] === ME_USER_EMAIL;
+      //       });
 
-            const { ratingsGroupedByUser, ratingsGroupedByBook } = prepareRatings([...ME_USER_RATINGS, ...ratings]);
-            const cfItemBasedRecommendation = predictWithCfItemBased(
-              ratingsGroupedByUser,
-              ratingsGroupedByBook,
-              ME_USER_ID
-            );
+      //       const { ratingsGroupedByUser, ratingsGroupedByBook } = prepareRatings([...ME_USER_RATINGS, ...ratings]);
+      //       const cfItemBasedRecommendation = predictWithCfItemBased(
+      //         ratingsGroupedByUser,
+      //         ratingsGroupedByBook,
+      //         ME_USER_ID
+      //       );
 
-            res.send(sliceAndDice(cfItemBasedRecommendation, BOOKS_BY_ID, 10, true));
-          });
-      } else if (req.body.rectype === 'UCF') {
-        const ME_USER_ID = req.body.userId;
-        const ME_USER_EMAIL = req.body.userEmail;
+      //       res.send(sliceAndDice(cfItemBasedRecommendation, BOOKS_BY_ID, 10, true));
+      //     });
+      // } else if (req.body.rectype === 'UCF') {
+      //   const ME_USER_ID = req.body.userId;
+      //   const ME_USER_EMAIL = req.body.userEmail;
 
-        let ratings = [];
+      //   let ratings = [];
 
-        db.collection('ratings')
-          .get()
-          .then(snapshot => {
-            snapshot.forEach(doc => {
-              let rating = { ...doc.data() };
-              ratings.push(rating);
-            });
+      //   db.collection('ratings')
+      //     .get()
+      //     .then(snapshot => {
+      //       snapshot.forEach(doc => {
+      //         let rating = { ...doc.data() };
+      //         ratings.push(rating);
+      //       });
 
-            let ME_USER_RATINGS = ratings.filter(rating => {
-              return rating['user_email'] === ME_USER_EMAIL;
-            });
+      //       let ME_USER_RATINGS = ratings.filter(rating => {
+      //         return rating['user_email'] === ME_USER_EMAIL;
+      //       });
 
-            const { ratingsGroupedByUser, ratingsGroupedByBook } = prepareRatings([...ME_USER_RATINGS, ...ratings]);
-            console.log('ratingsGroupedByUser:', ratingsGroupedByUser);
-            console.log('ratingsGroupedByBook:', ratingsGroupedByBook);
-            const cfUserBasedRecommendation = predictWithCfUserBased(
-              ratingsGroupedByUser,
-              ratingsGroupedByBook,
-              ME_USER_ID
-            );
+      //       const { ratingsGroupedByUser, ratingsGroupedByBook } = prepareRatings([...ME_USER_RATINGS, ...ratings]);
+      //       console.log('ratingsGroupedByUser:', ratingsGroupedByUser);
+      //       console.log('ratingsGroupedByBook:', ratingsGroupedByBook);
+      //       const cfUserBasedRecommendation = predictWithCfUserBased(
+      //         ratingsGroupedByUser,
+      //         ratingsGroupedByBook,
+      //         ME_USER_ID
+      //       );
 
-            res.send(sliceAndDice(cfUserBasedRecommendation, BOOKS_BY_ID, 10, true));
-          });
-      }
+      //       res.send(sliceAndDice(cfUserBasedRecommendation, BOOKS_BY_ID, 10, true));
+      //     });
+      // }
       let intentMap = new Map();
       intentMap.set('describe_book', describeBook);
       intentMap.set('similar_books', similarBooks);
+      intentMap.set('buy_book', addBookToCart);
+      intentMap.set('clear_cart', clearCart);
+      intentMap.set('author_books', sameAuthorBooks);
 
       agent.handleRequest(intentMap);
     });
